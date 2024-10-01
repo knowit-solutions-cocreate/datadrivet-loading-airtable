775ad4029a2ccd5d7b3d8d49fe2de81c301bee1a
MikaGie <mikaela.giegold@knowit.se>
2024-10-01 15:34:54 +0200

    feat: Add asset and checks for airtable
    BREAKING CHANGE:

--- dagster_project/assets/assets.py
+++ dagster_project/assets/assets.py
@@ -1,16 +1,21 @@
 from typing import Iterable
-from dlt import pipeline
-from dlt.extract.resource import DltResource
-from dlt.destinations import snowflake
+
 from dagster import AssetExecutionContext, AssetKey, SourceAsset
-from dagster_embedded_elt.dlt import DagsterDltResource, DagsterDltTranslator, dlt_assets
-from data_sources.pokemon import pokemon
+from dagster_embedded_elt.dlt import (
+    DagsterDltResource,
+    DagsterDltTranslator,
+    dlt_assets,
+)
+from dlt import pipeline
+from dlt.destinations import snowflake
+from dlt.extract.resource import DltResource
+
+from data_sources.airtable import airtable
 
 from ..constants import (
     source_base_name,
     source_schema_name,
 )
-
 
 pipeline_name = f"{source_base_name}_dlt_pipeline"
 
@@ -23,7 +28,7 @@ class DltPipelineTranslator(DagsterDltTranslator):
 
 
 @dlt_assets(
-    dlt_source=pokemon(),
+    dlt_source=airtable(),
     dlt_pipeline=pipeline(
         pipeline_name=pipeline_name,
         dataset_name=source_schema_name,
@@ -32,22 +37,21 @@ class DltPipelineTranslator(DagsterDltTranslator):
     ),
     name=f"{source_base_name}",
     group_name=f"{source_base_name}",
-    dagster_dlt_translator=DltPipelineTranslator()
+    dagster_dlt_translator=DltPipelineTranslator(),
 )
-def pokemon_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
+def airtable_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
     yield from dlt.run(context=context)
 
-    
+
 """
 Modifies metadata group name for the dlt_assets 
 to have depedencies in same group as assets.
 SourceAsset is deprecated but this does not work with AssetSpec yet.
 """
-pokemon_assets_source = [
+airtable_assets_source = [
     SourceAsset(
-        key=key, 
-        group_name=source_base_name, 
-        description=f"Dlt pipeline to load {source_base_name.capitalize()} data into {snowflake.__name__.capitalize()}."
-        ) 
-    for key in pokemon_assets.dependency_keys
-    ]
+        key=key,
+        group_name=source_base_name,
+    )
+    for key in airtable_assets.dependency_keys
+]
